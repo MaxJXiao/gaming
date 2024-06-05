060a81e3acdaeb807056ab2e6673da8924002dfa
MaxJXiao <80502041+MaxJXiao@users.noreply.github.com>
2024-06-05 22:31:21 +1000

    Add files via upload

--- chesspgn.jl
+++ chesspgn.jl
@@ -4,15 +4,32 @@
 
 1. e4 e6 2. Nf3 d5 3. Nc3 Nf6 4. e5 Nfd7 5. d4 c5 6. Bg5 Qb6 7. dxc5 Bxc5 8. Qd2 Bxf2+ 9. Qxf2 Qxb2 10. Kd2 Qxa1 11. Bb5 Qxh1 12. Qc5 Qxg2+ 13. Kc1 Nc6 14. Bxc6 f6 15. exf6 gxf6 16. Bxf6 Kf7 17. Qe7+ Kg6 18. Nh4+ Kh5 19. Nxg2 Nxf6 20. Qxf6 Bd7 21. Bxd7 Raf8 22. Qh4+ Kg6 23. Bxe6 Rf1+ 1-0
 
+
+1. e4 c5 2. Nf3 e6 3. d4 cxd4 4. Nxd4 a6 5. Nc3 b5 6. a3 Bb7 7. Bd3 Bc5 8. Nb3 Ba7 9. Qg4 Nf6 10. Qg3 d6 11. O-O Nc6 12. Qxg7 Rg8 13. Qh6 Ne5 14. Bg5 Rg6 15. Bxf6 Qxf6 16. Qxh7 Nf3+ 17. Kh1 Ke7 18. Qh3 Nxh2 19. Nd5+ Bxd5 20. exd5 Nxf1 21. Kg1 Rh8 22. Qxh8 Qxf2+ 23. Kh1 Rh6+ 24. Qxh6 Qg1# 0-1
+
 """
 
-pre = ["a" "b" "c" "d" "e" "f" "g" "h" "N" "B" "R" "Q" "K"]
+
+###########################################
+
 
 pgn₁ = "1. e4 e6 2. Nf3 d5 3. Nc3 Nf6 4. e5 Nfd7 5. d4 c5 6. Bg5 Qb6 7. dxc5 Bxc5 8. Qd2 Bxf2+ 9. Qxf2 Qxb2 10. Kd2 Qxa1 11. Bb5 Qxh1 12. Qc5 Qxg2+ 13. Kc1 Nc6 14. Bxc6 f6 15. exf6 gxf6 16. Bxf6 Kf7 17. Qe7+ Kg6 18. Nh4+ Kh5 19. Nxg2 Nxf6 20. Qxf6 Bd7 21. Bxd7 Raf8 22. Qh4+ Kg6 23. Bxe6 Rf1+ 1-0"
 
 moves_1 = find_sequence(pgn₁)
 
 white_gif_maker(moves_1)
+
+pgn₂ = "1. e4 c5 2. Nf3 e6 3. d4 cxd4 4. Nxd4 a6 5. Nc3 b5 6. a3 Bb7 7. Bd3 Bc5 8. Nb3 Ba7 9. Qg4 Nf6 10. Qg3 d6 11. O-O Nc6 12. Qxg7 Rg8 13. Qh6 Ne5 14. Bg5 Rg6 15. Bxf6 Qxf6 16. Qxh7 Nf3+ 17. Kh1 Ke7 18. Qh3 Nxh2 19. Nd5+ Bxd5 20. exd5 Nxf1 21. Kg1 Rh8 22. Qxh8 Qxf2+ 23. Kh1 Rh6+ 24. Qxh6 Qg1# 0-1"
+
+moves_2 = find_sequence(pgn₂)
+
+black_gif_maker(moves_2)
+
+
+##########################################
+
+
+pre = ["a" "b" "c" "d" "e" "f" "g" "h" "N" "B" "R" "Q" "K" "O"]
 
 
 """
@@ -86,4 +103,3 @@ end
 
 
 
-save(joinpath(@__DIR__, "images", "chess", "png" "$i.png"),white_board)
